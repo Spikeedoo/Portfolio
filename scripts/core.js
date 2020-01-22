1ab438311dd243e1f12e4b11159602ea175ab26f
Spikeedoo <spikeedoo14@gmail.com>
2020-01-21 19:56:21 -0600

    Initial Build (Grid + Styles + JS)

---
+++ scripts/core.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('#snap').click(function(){
+        $('#overlay').fadeIn("slow");
+        $('#maintainer').animate({opacity: "0.2"}, "slow");
+    });
+
+    // $('body').on("click", function(e){
+    //     var target = $(e.target);
+    //     if (target.parents("#maintainer").length){
+            
+    //     }
+    // });
+
+    $('#close').click(function(){
+        $('#overlay').fadeOut("slow");
+        $('#maintainer').animate({opacity: "1"}, "slow");
+    });
+});
